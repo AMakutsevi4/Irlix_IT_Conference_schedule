00670fa253db8a83e3b9996109c3f3bd61cfb09e
Alexandr <amakutsevich@bk.ru>
2024-08-09 16:35:55 +0700

    Init commit

---
+++ src/main/java/ru/irlix_it_conference_schedule/IrlixItConferenceScheduleApplication.java
@@ -0,0 +1,13 @@
+package ru.irlix_it_conference_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrlixItConferenceScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrlixItConferenceScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/irlix_it_conference_schedule/IrlixItConferenceScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.irlix_it_conference_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrlixItConferenceScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
